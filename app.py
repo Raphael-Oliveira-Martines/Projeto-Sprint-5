02c4cf3623d40bdfa195a0668cfc7b1ac753f67c
Raphael <raploliveira@gmail.com>
2026-03-08 16:31:45 -0300

    Adicionei o grafico de dispersão

--- app.py
+++ app.py
@@ -4,12 +4,18 @@ import plotly_express as px
 
 st.header('Vendas de carro')
 df_veiculos = pd.read_csv('vehicles.csv')
-hist_button = st.button('Criar histograma')  # criar um botão
 
-if hist_button:
+grafico_hist = st.checkbox("Criar um grafico histograma")
+grafico_disper = st.checkbox("Criar gráfico de dispersão")
+
+if grafico_hist:
     st.write(
         'Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
     fig = px.histogram(df_veiculos, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
 
-print(df_veiculos)
+if grafico_disper:
+    st.write(
+        'Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+    fig = px.scatter(df_veiculos, x="odometer", y="price")
+    st.plotly_chart(fig, use_container_width=True)
